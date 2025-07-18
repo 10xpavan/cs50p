ac64850b6e663c7ed03c2f069d01d244e38cd93a
10xpavan <pavan.nulenur@gmail.com>
2025-07-18 22:32:20 +0530

    finished lecture one, next problem sets are yet to be attacked!!

---
+++ lecture1/compare.py
@@ -0,0 +1,13 @@
+score = int(input("score:"))
+
+if 90 <= score <= 100:
+ print("grade: a")
+
+elif 80 <= score < 90:
+ print("grade: b")
+
+elif 60 <= score < 70:
+ print("grade: d")
+
+else:
+ print("grade: f")
